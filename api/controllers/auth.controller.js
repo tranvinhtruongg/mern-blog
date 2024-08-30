8e65cf3d59649e4f5223fca4974003c4bade1e56
tranvinhtruongg <101718796+tranvinhtruongg@users.noreply.github.com>
2024-08-30 16:23:42 +0700

    create signin API  route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,7 +1,9 @@
 import User from "../models/user.model.js";
 import bcryptjs from 'bcryptjs';
 import { errorHandler } from "../utils/error.js";
+import jwt from 'jsonwebtoken'
 
+// dang ky
 export const signup = async (req,res,next)=>{
     const {username,email,password}=req.body;
 
@@ -27,3 +29,33 @@ export const signup = async (req,res,next)=>{
     }
 
 }
+
+//dang nhap
+
+export const signin = async(req,res,next)=>{
+    const {email,password} = req.body
+    if(!email || !password || email==='' || password===''){
+        next(errorHandler(400,'Vui lòng nhập'))
+    }
+    try{
+        const validUser = await User.findOne({email})
+        if(!validUser){
+            return next(errorHandler(404,'Không có tên người dùng này'))
+        }
+        const validPassword = bcryptjs.compareSync(password, validUser.password);
+        if (!validPassword) {
+          return next(errorHandler(400, 'Mật khẩu không chính xác!'));
+        }
+        const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+        const { password: pass, ...rest } = validUser._doc
+        res.status(200)
+        .cookie('access_token', token, {
+          httpOnly: true,
+        })
+        .json(rest)
+    }
+    catch(err){
+        next(err)
+    }
+}
+
