abcf3f792e9fab032edd0114bd38693482037d9d
tranvinhtruongg <101718796+tranvinhtruongg@users.noreply.github.com>
2024-09-06 21:37:02 +0700

    add redux toolkit

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,12 +1,14 @@
 import { Alert, Button, Label, Spinner, TextInput } from "flowbite-react";
 import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
+import { useDispatch ,useSelector} from "react-redux";
+import { SignInStart,signInSuccess,signInFailure } from "../redux/user/userSlice";
 
 
 export default function SignIn() {
   const [formData,setFormData] = useState({})
-  const [errorMessage, setErrorMessage] = useState(null)
-  const [loading,setLoading] = useState(false)
+  const {loading,error:errorMessage}=useSelector(state =>state.user)
+  const dispatch = useDispatch()
   const navigate = useNavigate()
   const handleChange = (e)=>{
     setFormData({...formData,[e.target.id]:e.target.value.trim()})
@@ -14,11 +16,10 @@ export default function SignIn() {
   const handleSubmit =async (e)=>{
     e.preventDefault()
     if( !formData.email || !formData.password){
-      return setErrorMessage('Vui lòng điền hết tất cả mục.')
+      return dispatch(signInFailure('Vui lòng điền đầy đủ thông tin'))
     }
     try{
-      setLoading(true)
-      setErrorMessage(null)
+      dispatch(SignInStart())
       const res = await fetch('/api/auth/signin',{
         method:'POST',
         headers:{'Content-Type': 'application/json'},
@@ -26,16 +27,15 @@ export default function SignIn() {
       });
       const data = await res.json();
       if (data.success === false) {
-        return setErrorMessage(data.message);
+        dispatch(signInFailure(data.message));
       }
-      setLoading(false)
       if(res.ok){
+        dispatch(signInSuccess(data))
         navigate('/')
       }
     }
     catch(err){
-      setErrorMessage(err.message);
-      setLoading(false);
+      dispatch(signInFailure(err.message))
     }
   }
   
