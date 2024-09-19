73a01ac2703059a913e01479686ca9cb950ff9f9
tranvinhtruongg <101718796+tranvinhtruongg@users.noreply.github.com>
2024-09-19 11:10:45 +0700

    add update user api route

--- api/models/user.model.js
+++ api/models/user.model.js
@@ -5,18 +5,15 @@ const userSchema = new mongoose.Schema({
         type:String,
         required:true,
         unique:true,
-        minlength: 6
     },
     email:{
         type:String,
         required:true,
         unique:true,
-        match:/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
     },
     password:{
         type: String,
         required:true,
-        minlength: 8
     },
     profilePicture:{
         type:String,
