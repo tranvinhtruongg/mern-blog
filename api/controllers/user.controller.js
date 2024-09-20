d5880c855d839c878cba0c8c6f9e514ad4ccb518
tranvinhtruongg <101718796+tranvinhtruongg@users.noreply.github.com>
2024-09-20 15:22:05 +0700

    complete update user profile page functionality

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -3,12 +3,12 @@ import { errorHandler } from '../utils/error.js';
 import User from '../models/user.model.js';
 
 export const test = (req, res) => {
-  res.json({ message: 'API is working!' });
+  res.json({ message: 'API đang hoạt động!' });
 };
 
 export const updateUser = async (req, res, next) => {
   if (req.user.id !== req.params.userId) {
-    return next(errorHandler(403, 'Bạn không được thay đổi người dùng này'));
+    return next(errorHandler(403, 'Bạn không được phép cập nhật người dùng này'));
   }
   if (req.body.password) {
     if (req.body.password.length < 6) {
@@ -23,33 +23,33 @@ export const updateUser = async (req, res, next) => {
       );
     }
     if (req.body.username.includes(' ')) {
-      return next(errorHandler(400, 'Tên người dùng không được có khoảng trắng'));
+      return next(errorHandler(400, 'Tên người dùng không được chứa khoảng trắng'));
     }
     if (req.body.username !== req.body.username.toLowerCase()) {
       return next(errorHandler(400, 'Tên người dùng phải viết thường'));
     }
     if (!req.body.username.match(/^[a-zA-Z0-9]+$/)) {
       return next(
-        errorHandler(400, 'Tên người dùng chỉ được là chữ cái và số')
+        errorHandler(400, 'Tên người dùng chỉ được chứa chữ cái và số')
       );
     }
-    try {
-      const updatedUser = await User.findByIdAndUpdate(
-        req.params.userId,
-        {
-          $set:{
-            username: req.body.username,
-            email: req.body.email,
-            profilePicture: req.body.profilePicture,
-            password: req.body.password,
-          },
+  }
+  try {
+    const updatedUser = await User.findByIdAndUpdate(
+      req.params.userId,
+      {
+        $set: {
+          username: req.body.username,
+          email: req.body.email,
+          profilePicture: req.body.profilePicture,
+          password: req.body.password,
         },
-        { new: true }
-      );
-      const { password, ...rest } = updatedUser._doc;
-      res.status(200).json(rest);
-    } catch (error) {
-      next(error);
-    }
+      },
+      { new: true }
+    );
+    const { password, ...rest } = updatedUser._doc;
+    res.status(200).json(rest);
+  } catch (error) {
+    next(error);
   }
 };
--- client/src/conponents/DashProfile.jsx
+++ client/src/conponents/DashProfile.jsx
@@ -5,6 +5,8 @@ import {getDownloadURL, getStorage,ref,uploadBytesResumable} from 'firebase/stor
 import { app } from '../firebase';
 import { CircularProgressbar } from 'react-circular-progressbar';
 import 'react-circular-progressbar/dist/styles.css';
+import { updateStart,updateFailure,updateSuccess } from '../redux/user/userSlice';
+import { useDispatch } from 'react-redux';
 
 export default function DashProfile() {
     const { currentUser } = useSelector((state) => state.user);
@@ -12,60 +14,115 @@ export default function DashProfile() {
     const [imageFileUrl, setImageFileUrl] = useState(null);
     const [imageFileUploadProgress, setImageFileUploadProgress] = useState(null);
     const [imageFileUploadError, setImageFileUploadError] = useState(null);
-    const filePickerRef = useRef()
-    const handleImageChange = (e)=>{
-        const file =e.target.files[0]
-        if(file){
-            setImageFile(file)
-            setImageFileUrl(URL.createObjectURL(file))
+    const [imageFileUploading, setImageFileUploading] = useState(false);
+    const [updateUserSuccess, setUpdateUserSuccess] = useState(null);
+    const [updateUserError, setUpdateUserError] = useState(null);
+    const [formData, setFormData] = useState({});
+    const filePickerRef = useRef();
+    const dispatch = useDispatch();
+    const handleImageChange = (e) => {
+        const file = e.target.files[0];
+        if (file) {
+          setImageFile(file);
+          setImageFileUrl(URL.createObjectURL(file));
         }
-        
-    }
-    useEffect(()=>{
-        if(imageFile){
-            uploadImage()
+      };
+      useEffect(() => {
+        if (imageFile) {
+          uploadImage();
         }
-    },[imageFile])
-
-    const uploadImage = async ()=>{
-        // service firebase.storage {
-        //     match /b/{bucket}/o {
-        //       match /{allPaths=**} {
-        //         allow read
-        //         allow write:if
-        //         request.resource.size<2*1024*1024 &&
-        //         request.resource.contentType.matches("image/.*")
-        //       }
-        //     }
-        //   }
-        setImageFileUploadError(null)
-        const storage = getStorage(app);
-        const fileName = new Date().getTime() + imageFile.name;
-        const storageRef = ref(storage,fileName)
-        const uploadTask = uploadBytesResumable(storageRef, imageFile);
-        uploadTask.on('state_changed',(snapshot)=>{
-            const progress = 
+      }, [imageFile]);
+    
+    const uploadImage = async () => {
+    // service firebase.storage {
+    //   match /b/{bucket}/o {
+    //     match /{allPaths=**} {
+    //       allow read;
+    //       allow write: if
+    //       request.resource.size < 2 * 1024 * 1024 &&
+    //       request.resource.contentType.matches('image/.*')
+    //     }
+    //   }
+    // }
+    setImageFileUploading(true);
+    setImageFileUploadError(null);
+    const storage = getStorage(app);
+    const fileName = new Date().getTime() + imageFile.name;
+    const storageRef = ref(storage, fileName);
+    const uploadTask = uploadBytesResumable(storageRef, imageFile);
+    uploadTask.on(
+        'state_changed',
+        (snapshot) => {
+        const progress =
             (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-            setImageFileUploadProgress(progress.toFixed(0))
-        },(error)=>{
-            setImageFileUploadError('không thể tải hình ảnh lên(tập tin phải nhỏ hơn 2MB)')
-            setImageFileUploadProgress(null)
-            setImageFile(null)
-            setImageFileUrl(null) 
+
+        setImageFileUploadProgress(progress.toFixed(0));
         },
-        ()=>{
-            getDownloadURL(uploadTask.snapshot.ref).then((downloadURL)=>{
-                setImageFileUrl(downloadURL)
-            })
+        (error) => {
+        setImageFileUploadError(
+            'Không thể tải lên hình ảnh (Tệp phải nhỏ hơn 2MB)'
+        );
+        setImageFileUploadProgress(null);
+        setImageFile(null);
+        setImageFileUrl(null);
+        setImageFileUploading(false);
+        },
+        () => {
+        getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+            setImageFileUrl(downloadURL);
+            setFormData({ ...formData, profilePicture: downloadURL });
+            setImageFileUploading(false);
+        });
         }
-    )
+    );
+    };
+    
+
+    const handleChange = (e)=>{
+        setFormData({...formData,[e.target.id]:e.target.value})
     }
+    
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        setUpdateUserError(null);
+        setUpdateUserSuccess(null);
+        if (Object.keys(formData).length === 0) {
+          setUpdateUserError('Không có thay đổi nào được thực hiện');
+          return;
+        }
+        if (imageFileUploading) {
+          setUpdateUserError('Vui lòng chờ tải lên hình ảnh');
+          return;
+        }
+        try {
+          dispatch(updateStart());
+          const res = await fetch(`/api/user/update/${currentUser._id}`, {
+            method: 'PUT',
+            headers: {
+              'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(formData),
+          });
+          const data = await res.json();
+          if (!res.ok) {
+            dispatch(updateFailure(data.message));
+            setUpdateUserError(data.message);
+          } else {
+            dispatch(updateSuccess(data));
+            setUpdateUserSuccess("Cập nhật hồ sơ người dùng thành công");
+          }
+        } catch (error) {
+          dispatch(updateFailure(error.message));
+          setUpdateUserError(error.message);
+        }
+      };
   return (
     <div className='max-w-lg mx-auto p-3 w-full'>
         <h1 className='my-7 text-center font-semibold text-3xl'>
             profile
         </h1>
-        <form className='flex flex-col gap-4'>
+        <form onSubmit={handleSubmit} className='flex flex-col gap-4'>
             <input type='file' accept='image/*' onChange={handleImageChange} ref={filePickerRef} hidden/>
             <div className="relative w-32 h-32 self-center cursor-pointer shadow-md overflow-hidden rounded-full " 
             onClick={()=>filePickerRef.current.click()}>
@@ -90,15 +147,25 @@ export default function DashProfile() {
                     {imageFileUploadError}
                 </Alert>
             }
-            <TextInput type='text' id='username' placeholder='Tên người dùng' defaultValue={currentUser.username}/>
-            <TextInput type='email' id='email' placeholder='email' defaultValue={currentUser.email}/>
-            <TextInput type='password' id='password' placeholder='Mật khẩu' />
+            <TextInput type='text' id='username' placeholder='Tên người dùng' defaultValue={currentUser.username} onChange={handleChange}/>
+            <TextInput type='email' id='email' placeholder='email' defaultValue={currentUser.email} onChange={handleChange}/>
+            <TextInput type='password' id='password' placeholder='Mật khẩu' onChange={handleChange} />
             <Button type='submit'  gradientDuoTone='purpleToBlue' outline>Cập nhật</Button>
         </form>
         <div className="text-red-500 flex justify-between mt-5">
             <span className='cursor-pointer'>Xóa tài khoản</span>
             <span className='cursor-pointer'>Đăng xuất</span>
         </div>
+        {updateUserSuccess && (
+        <Alert color='success' className='mt-5'>
+          {updateUserSuccess}
+        </Alert>
+        )}
+        {updateUserError && (
+            <Alert color='failure' className='mt-5'>
+            {updateUserError}
+            </Alert>
+        )}
     </div>
   )
 }
