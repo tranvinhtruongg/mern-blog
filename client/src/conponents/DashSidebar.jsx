102547ed2de5b96c35cb1b1c99325925b7d39c3a
tranvinhtruongg <101718796+tranvinhtruongg@users.noreply.github.com>
2024-12-18 00:17:15 +0700

    fix

--- client/src/conponents/DashSidebar.jsx
+++ client/src/conponents/DashSidebar.jsx
@@ -40,13 +40,6 @@ export default function DashSidebar() {
     <Sidebar className='w-full md:w-56'>
         <Sidebar.Items>
             <Sidebar.ItemGroup className='flex flex-col gap-1'>
-            {currentUser.isAdmin && (
-              <Link to='/dashboard?tab=visitor'>
-                <Sidebar.Item active={tab==='visitor'} icon={HiDocumentText} as='div'>
-                  Thống kê lượt truy cập
-                </Sidebar.Item>
-              </Link>
-            )}
             {currentUser && currentUser.isAdmin && (
               <Link to='/dashboard?tab=dash'>
                 <Sidebar.Item
